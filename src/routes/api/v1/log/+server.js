e7b12393903a59fc6d42a1eab74cd79e03a853c6
운구름 <wooncloud@hotmail.co.kr>
2024-03-26 01:54:10 +0900

    서버로그 api 기반 추가

---
+++ src/routes/api/v1/log/+server.js
@@ -0,0 +1,6 @@
+import { supabase } from "$lib/supabaseClient";
+import { json } from "@sveltejs/kit";
+
+export async function POST({ request }) {
+    // 로그 받아서 서버 로그로 넣기
+}
---
+++ src/routes/guest-book/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    // your script goes here
+</script>
+
+<style>
+    /* your styles go here */
+</style>
+
+<section>
+    <!-- 여기는 사용자가 메시지를 입력할 수 있도록 함. -->
+    <!-- 예시도 넣어주자 -->
+    <!-- 오늘 재택근무! -->
+    <!-- 오늘 연차입니다 -->
+</section>
