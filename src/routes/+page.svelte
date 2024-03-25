4c709c5f54b8d88e043ab07037fcf1422b519aa3
운구름 <wooncloud@hotmail.co.kr>
2024-03-26 01:08:05 +0900

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,72 @@
+<script>
+    import { supabase } from "$lib/supabase";
+    import { Container, Col, Row } from '@sveltestrap/sveltestrap';
+    import GuestBook from './GuestBook.svelte';
+    import ServerLog from './ServerLog.svelte';
+
+    let guestBooks = [];
+    let serverLogs = [];
+    $: guestBooks;
+    $: serverLogs;
+
+    async function getGuestBook() {
+        let { data: GUEST_BOOK, error } = await supabase
+            .from('GUEST_BOOK')
+            .select('*')
+            .order('created_at', { ascending: false })
+            .limit(10);
+
+        if (error) {
+            console.error(error);
+            return;
+        }
+
+        guestBooks = GUEST_BOOK;
+    }
+
+    async function getServerLog() {
+        let { data: SERVER_LOG, error } = await supabase
+            .from('SERVER_LOG')
+            .select('*')
+            .order('created_at', { ascending: false })
+            .limit(10);
+          
+        if (error) {
+            console.error(error);
+            return;
+        }
+
+        serverLogs = SERVER_LOG;
+    }
+
+    getGuestBook();
+    getServerLog();
+
+    supabase.channel('guest-book-channel')
+        .on('postgres_changes', { event: '*', schema: 'public', table: 'GUEST_BOOK' }, getGuestBook)
+        .subscribe();
+
+    supabase.channel('server-log-channel')
+        .on('postgres_changes', { event: '*', schema: 'public', table: 'SERVER_LOG' }, getServerLog)
+        .subscribe();
+</script>
+
+<style>
+    
+</style>
+
+<Container fluid>
+    <Row>
+        <Col sm="3">
+            {#each guestBooks as gb (gb.uid)}
+            <GuestBook name={gb.user_name} message={gb.message} color={gb.color} date={gb.created_at}/>
+            {/each}
+        </Col>
+        <Col sm="6">2</Col>
+        <Col sm="3">
+            {#each serverLogs as sl (sl.id)}
+            <ServerLog title={sl.title} message={sl.message} color={sl.color} date={sl.created_at}/>
+            {/each}
+        </Col>
+    </Row>
+</Container>
---
+++ src/routes/ServerLog.svelte
@@ -0,0 +1,47 @@
+<script>
+    import { Alert } from '@sveltestrap/sveltestrap';
+    import { fly } from 'svelte/transition';
+    export let title = '';
+    export let message = '';
+    export let color = '';
+    export let date = '';
+    const formatDate = getDateTimeFormet(date);
+
+    function getDateTimeFormet(date) {
+        const options = {
+            year: "2-digit",
+            month: "short",
+            weekday: "short",
+            day: "numeric",
+            hour: "numeric",
+            minute: "numeric",
+            second: "numeric",
+        }
+        const intl = new Intl.DateTimeFormat("ko-KR", options);
+        const dateObj = new Date(date);
+
+        return intl.format(dateObj);
+    }
+</script>
+
+<style>
+    .card-wrap {
+        margin: 1rem 0;
+    }
+
+    .date-font {
+        font-size: small;
+        opacity: 0.8;
+    }
+</style>
+
+<div class="card-wrap" transition:fly={{ y: -25, duration: 1000 }}>
+    <Alert 
+        color={color}
+        heading={title}
+        theme="dark"
+    >
+        <div class="date-font">{formatDate}</div>
+        <div>{message}</div>
+    </Alert>
+</div>
