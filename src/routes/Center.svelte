2d9e21e06d533aa4eb345d8bf054375b27042313
운구름 <wooncloud@hotmail.co.kr>
2024-03-26 01:53:26 +0900

    센터 컴포넌트

---
+++ src/routes/Center.svelte
@@ -0,0 +1,29 @@
+<script>
+    import { Card } from '@sveltestrap/sveltestrap';
+    import { Col, Row } from '@sveltestrap/sveltestrap';
+
+</script>
+
+<style>
+    section {
+        padding: 1rem 0;
+    }
+</style>
+
+<section>
+    <!-- 각종 위젯 -->
+    <Row class="mb-3">
+        <Col>
+            <Card body inverse color="dark">api 센터 핑 위젯</Card>
+        </Col>
+        <Col><Card body inverse color="dark">아무 위젯</Card></Col>
+        <Col><Card body inverse color="dark">아무 위젯</Card></Col>
+    </Row>
+    <Row class="mb-3">
+        <Col>
+            <Card body inverse color="dark">아무 위젯</Card>
+        </Col>
+        <Col><Card body inverse color="dark">아무 위젯</Card></Col>
+        <Col><Card body inverse color="dark">아무 위젯</Card></Col>
+    </Row>
+</section>
